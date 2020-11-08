a40734fdede5195d8f0d358525451fa48b0433a3
jodene garvey <jodenepatrice@hotmail.com>
2020-11-07 20:57:09 -0600

    Jodene - Web Page Design

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -7,18 +7,47 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace JTB_Airline_and_Cruise {
-    
-    
-    public partial class SiteMaster {
-        
+namespace JTB_Airline_and_Cruise
+{
+
+
+    public partial class SiteMaster
+    {
+
         /// <summary>
-        /// MainContent control.
+        /// head control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image image1;
+
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
